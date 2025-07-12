b3511281ab184f3bef389e886aea4e6306472ea8
Mansill Smith <mansill.smith@gmail.com>
2025-07-12 22:16:53 +1200

    adding error handling to person player

--- Players/PersonPlayer.py
+++ Players/PersonPlayer.py
@@ -1,5 +1,6 @@
 from .Player import Player
 from Move import Move
+from CellValues import CellValues
 
 class PersonPlayer(Player):
     def ValidateUserMove(self, input:str):
@@ -10,15 +11,19 @@ class PersonPlayer(Player):
         try:
             x = int(splits[0])
             y = int(splits[1])
-            return Move(x,y)
+
+            if x >= 0 and x <= (self.game.size-1) and y >=0 and y <= (self.game.size-1):
+                return Move(x,y)
         except:
-            return None
+            pass
+
+        return None
         
     def DoMove(self):
         while True:
             userInput = input()
             move = self.ValidateUserMove(userInput)
-            if move is not None:
+            if move is not None and self.game.boardArray[move.y][move.x] == CellValues.EMPTY:
                 super().PlayMove(move)
                 return
             else:
