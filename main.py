3a22712ffeafa5f16a80cfb64554939b9531dc07
Mansill Smith <mansill.smith@gmail.com>
2025-07-13 20:12:04 +1200

    adding extra computers

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from TicTacToeGame import TicTacToeGame
 from Players.PersonPlayer import PersonPlayer
 from Players.ComputerPlayerRandom import ComputerPlayerRandom
 from Players.ComputerPlayerWinIfItCan import ComputerPlayerWinIfItCan
+from Players.ComputerPlayerDefensive import ComputerPlayerDefensive
+from Players.ComputerPlayerOffensiveAndDefensive import ComputerPlayerOffensiveAndDefensive
 from CellValues import CellValues
 
 def clear_terminal():
@@ -14,7 +16,11 @@ print(game)
 
 # players = [PersonPlayer(CellValues.NOUGHT, game), ComputerPlayerPickNext(CellValues.CROSS, game)]
 # players = [PersonPlayer(CellValues.NOUGHT, game), ComputerPlayerRandom(CellValues.CROSS, game)]
-players = [PersonPlayer(CellValues.NOUGHT, game), ComputerPlayerWinIfItCan(CellValues.CROSS, game)]
+# players = [PersonPlayer(CellValues.NOUGHT, game), ComputerPlayerWinIfItCan(CellValues.CROSS, game)]
+
+p1 = PersonPlayer(CellValues.NOUGHT, game)
+# players = [p1, ComputerPlayerDefensive(CellValues.CROSS, game, p1)]
+players = [ComputerPlayerOffensiveAndDefensive(CellValues.CROSS, game, p1), p1]
 turnNumber = 0
 while not game.IsGameWon() and not game.IsBoardFull():
     players[turnNumber % len(players)].DoMove()
